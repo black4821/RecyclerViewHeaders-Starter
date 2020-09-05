54a3219ba2f07af82296c43b5cfec77582256585
black4821 <56718434+black4821@users.noreply.github.com>
2020-09-05 20:53:15 +0700

    Step 3-4-7.4

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -135,6 +135,7 @@ class SleepTrackerFragment : Fragment() {
         val manager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = manager
 
+
         return binding.root
     }
 }
